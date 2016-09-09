a8902ab9f4f8e0904bd0cfdd6a2045f66577df6c
TaegangersAon <TaegangersAon@DESKTOP-LF0HH7D>
2016-09-09 14:31:58 +0700

    Add project files.

---
+++ Hello_OpenSource/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace Hello_OpenSource
+{
+    class Hello
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello My name is Anongnard  Namchompoo 5711423114");
+
+            //Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
